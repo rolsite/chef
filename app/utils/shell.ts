c45d8886f4993eded97560e33d28e8f818e76ee1
Sujay Jayakar <sujayakar@convex.dev>
2025-04-03 09:57:38 -0400

    Have lazier startup for faster TTI

--- app/utils/shell.ts
+++ app/utils/shell.ts
@@ -2,8 +2,12 @@ import type { WebContainer, WebContainerProcess } from '@webcontainer/api';
 import type { ITerminal } from '~/types/terminal';
 import { withResolvers } from './promises';
 import { atom } from 'nanostores';
+import { ContainerBootState, waitForContainerBootState } from '~/lib/webcontainer';
 
 export async function newShellProcess(webcontainer: WebContainer, terminal: ITerminal) {
+  // Wait for setup to fully complete before allowing shells to spawn.
+  await waitForContainerBootState(ContainerBootState.READY);
+
   const args: string[] = [];
 
   // we spawn a JSH process with a fallback cols and rows in case the process is not attached yet to a visible terminal
