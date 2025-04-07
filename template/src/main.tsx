227348f956f13cdb04e37445309d2320006284a1
Sarah Shader <sarahshader@gmail.com>
2025-04-07 16:33:49 -0700

    wip

--- template/src/main.tsx
+++ template/src/main.tsx
@@ -5,17 +5,11 @@ import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
 
-const storage = {
-  getItem: (key: string) => window.sessionStorage.getItem(key),
-  setItem: (key: string, value: string) => window.sessionStorage.setItem(key, value),
-  removeItem: (key: string) => window.sessionStorage.removeItem(key),
-};
-
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConvexAuthProvider client={convex} storage={storage}>
+    <ConvexAuthProvider client={convex} storage={window.sessionStorage}>
       <App />
     </ConvexAuthProvider>
   </StrictMode>,
