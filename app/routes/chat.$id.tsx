2de292fefdb21bb45e4cc10f634491928a95e598
Tom Ballinger <tom@convex.dev>
2025-04-08 16:53:08 -0700

    a few fixes

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunctionArgs } from '@vercel/remix';
+import { json, type LoaderFunctionArgs } from '@vercel/remix';
 import { default as IndexRoute, meta as IndexMeta } from './_index';
 import { getFlexAuthModeInLoader } from '~/lib/persistence/convex';
 
@@ -8,7 +8,7 @@ export async function loader(args: LoaderFunctionArgs) {
   const flexAuthMode = getFlexAuthModeInLoader();
   const url = new URL(args.request.url);
   const code = url.searchParams.get('code');
-  return Response.json({ id: args.params.id, flexAuthMode, code });
+  return json({ id: args.params.id, flexAuthMode, code });
 }
 
 export default IndexRoute;
